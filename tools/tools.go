2d0c8411ff97f3612c50018e3a32cb06055a4865
Connection Refused <cutecr@pm.me>
2021-09-08 13:14:44 +0800

    Update buffer size

--- tools/tools.go
+++ tools/tools.go
@@ -27,12 +27,12 @@ func IPToIPNet(addr net.IP) (n *net.IPNet) {
 
 func CopyBuffer(client, remote net.Conn) {
 	go func() {
-		_, _ = io.CopyBuffer(remote, client, make([]byte, 1400))
+		_, _ = io.CopyBuffer(remote, client, make([]byte, 1446))
 		_ = client.SetDeadline(time.Now())
 		_ = remote.SetDeadline(time.Now())
 	}()
 
-	_, _ = io.CopyBuffer(client, remote, make([]byte, 1400))
+	_, _ = io.CopyBuffer(client, remote, make([]byte, 1446))
 	_ = client.SetDeadline(time.Now())
 	_ = remote.SetDeadline(time.Now())
 }
