6d5e7d365c868f83daa13640fc7d103fa3df7151
Connection Refused <cutecr@pm.me>
2021-09-20 17:22:39 +0800

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,17 +1,14 @@
-if (Test-Path release) {
+if ( Test-Path release ) {
     rm -Recurse -Force release | Out-Null
 }
 
-$Env:CGO_ENABLED="0"
-$Env:GOROOT_FINAL="/usr"
+$Env:CGO_ENABLED='0'
+$Env:GOROOT_FINAL='/usr'
 
-$Env:GOOS="linux"
-$Env:GOARCH="amd64"
-go build -a -trimpath -asmflags "-s -w" -ldflags "-s -w" -o release\stream
+$Env:GOOS='linux'
+$Env:GOARCH='amd64'
+go build -a -trimpath -asmflags '-s -w' -ldflags '-s -w' -o release\stream
 if ( -Not $? ) { exit $lastExitCode }
 
-upx --ultra-brute release\stream
-if ( -Not $? ) { exit $lastExitCode }
-
-cp -Force example.json release
+cp -Force default.json release
 exit 0
